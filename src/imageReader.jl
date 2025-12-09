b3b198fb2f54742c2580242f0ca05b91eb3ad426
Nobuaki Fuji <seismobassoon@gmail.com>
2025-12-09 17:13:09 +0100

    hesa

--- src/imageReader.jl
+++ src/imageReader.jl
@@ -2,9 +2,12 @@ module imageReader
     using FileIO,CairoMakie
 
     include("batchFiles/batchImages.jl")
-
     include("imageReader/imageReader.jl")
+    include("imageReader/modelMaker.jl")
 
-    export read2DimageModel, defineModel
+
+    export defineModel
+    export read2DimageModel
+
 
 end
