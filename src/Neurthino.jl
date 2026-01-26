ef84af333491b0c6c46ccfc3bce0f1055bd46281
Nobuaki Fuji <seismobassoon@gmail.com>
2026-01-26 17:01:09 +0100

    partir

--- src/Neurthino.jl
+++ src/Neurthino.jl
@@ -23,6 +23,10 @@ module Neurthino
     const N_A = 6.022e23 #[mol^-1]
     const G_F = 8.961877245622253e-38 #[eV*cm^3]
 
+    DEFAULT_PLANET = Ref(:Earth)
+
+
+
     # Julia 1.0 compatibility - normally obsolete
     #isnothing(::Any) = false
     #isnothing(::Nothing) = true
@@ -30,12 +34,12 @@ module Neurthino
     #specific sources
 
     include("Neurthino/Oscillation.jl")
-    include("Neurthino/Matter.jl")
-    include("Neurthino/earthModels2Dor3D.jl")
-    include("Neurthino/Earthmodels.jl")
-    include("Neurthino/premFunctions.jl")
-    include("Neurthino/usefulFunctionsToPlot.jl")
-    include("Neurthino/NeurthinoRelated.jl")
+    #include("Neurthino/Matter.jl")
+    #include("Neurthino/earthModels2Dor3D.jl")
+    #include("Neurthino/Earthmodels.jl")
+    #include("Neurthino/premFunctions.jl")
+    #include("Neurthino/usefulFunctionsToPlot.jl")
+    #include("Neurthino/NeurthinoRelated.jl")
 
 
     
