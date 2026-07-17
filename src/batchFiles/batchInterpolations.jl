5b3412756039ec50c68e9a3d96e503d4e126c7ee
Nobuaki Fuji <seismobassoon@gmail.com>
2026-07-17 17:00:04 +0200

    inti

--- src/batchFiles/batchInterpolations.jl
+++ src/batchFiles/batchInterpolations.jl
@@ -8,28 +8,31 @@ epsilon2Default =1.;
 giveMaskPX(Xs,Ys)=giveMaskPX(Xs,Ys,nothing)
 
 function giveMaskPX(Xs,Ys,Zs)
+
+    minX, maxX, nX = @unpack Xs
+    minY, maxY, nY = @unpack Ys
+
     if Zs === nothing
 
-        minZ=0.0
-        maxZ=0.0
-        tmpX=correlationLength[1]
-        tmpY=correlationLength[2]
-
-        #correlationLength=(tmpX,tmpY)
-
-        return mask,(pm,pn),(xi,yi) = DIVAnd_rectdom(range(minX,stop=maxX,length=nX),
-                                                range(minY,stop=maxY,length=nY));
+        mask,(pm,pn),(xi,yi) = DIVAnd_rectdom(range(minX,stop=maxX,length=nX),range(minY,stop=maxY,length=nY))
+        return (mask=mask,Ps=(pm,pn),XIs=(xi,yi))
+                                            
     else
-
-        return mask,(pm,pn,po),(xi,yi,zi) = DIVAnd_rectdom(range(minX,stop=maxX,length=nX),
-                                                range(minY,stop=maxY,length=nY),
-                                                range(minZ,stop=maxZ,length=nZ));
+        minZ, maxZ, nZ = @unpack Zs
+        mask,(pm,pn,po),(xi,yi,zi) = DIVAnd_rectdom(range(minX,stop=maxX,length=nX),range(minY,stop=maxY,length=nY),range(minZ,stop=maxZ,length=nZ))
+        return (mask=mask,Ps=(pm,pn,po),XIs=(xi,yi,zi))
     end
 end
 
+interpolateField(Xs,Ys,Xnode,Ynode;correlationLength=correlationLengthDefault,epsilon2=epsilon2Default) = interpolateField(Xs,Ys,nothing,Xnode,Ynode,nothing;correlationLength=correlationLength,epsilon2=epsilon2)
 
-function interpolate2D3DField(;correlationLength=correlationLengthDefault,epsilon2=epsilon2Default) 
-    mask, Ps, XIs = giveMaskPX()
-
-fieldCartesien,_ =DIVAndrun(mask,(pm,pn),(xi,yi),(Xnode,Ynode),diffρField,correlationLength,epsilon2)
+function interpolateField(Xs,Ys,Zs,XXnodes;correlationLength=correlationLengthDefault,epsilon2=epsilon2Default) 
+    masks = giveMaskPX(Xs,Ys,Zs)
+    if Znode === nothing
+        XXnodes = (Xnode,Ynode)
+    else
+        XXnodes = (Xnode,Ynode,Znode)
+    end
+    fieldCartesien,_ =DIVAndrun(masks.mask,masks.Ps,masks.XIs,XXnodes,rawField,correlationLength,epsilon2)
+    return fieldCartesien
 end
